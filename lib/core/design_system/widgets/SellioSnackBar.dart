1cc7e763336edc99d288355691f6a737fb83f9da
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-11-05 14:31:18 +0200

    feat: use material snack bar component

--- lib/core/design_system/widgets/SellioSnackBar.dart
+++ lib/core/design_system/widgets/SellioSnackBar.dart
@@ -1,75 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class SellioSnackBar extends StatelessWidget {
-  final bool isError;
-  final String title;
-  final String message;
-
-  const SellioSnackBar({
-    super.key,
-    required this.isError,
-    required this.title,
-    required this.message,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 62,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
-        borderRadius: BorderRadius.circular(8),
+class SellioSnackBar {
+  static SnackBar show({
+    required bool isError,
+    required String title,
+    required String message,
+    required BuildContext context,
+  }) {
+    return SnackBar(
+      margin: EdgeInsetsGeometry.directional(
+          bottom: MediaQuery.of(context).size.height - 200
       ),
-      child: Stack(
-        children: [
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 46, height: 46,
-                decoration: BoxDecoration(
-                  color: isError
-                      ? Theme.of(context).disabledColor
-                      : Theme.of(context).disabledColor,
-                  borderRadius: BorderRadius.circular(4),
+      backgroundColor: Colors.transparent,
+      behavior: SnackBarBehavior.floating,
+      elevation: 0,
+      content: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Stack(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 46,
+                  height: 46,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).disabledColor,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: SvgPicture.asset(
+                    width: 32,
+                    height: 32,
+                    isError
+                        ? 'assets/icons/ic_alert.svg'
+                        : 'assets/icons/ic_success.svg',
+                  ),
                 ),
-                child: SvgPicture.asset(
-                  width: 32, height: 32,
-                  isError
-                      ? 'assets/icons/ic_alert.svg'
-                      : 'assets/icons/ic_success.svg',
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(message),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(width: 8),
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [Text(title), Text(message)]
-              ),
-            ],
-          ),
-          Positioned(
-            top: 8, right: 8,
-            child: GestureDetector(
-              onTap: () {},
-              child: Container(
-                width: 24, height: 24,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).disabledColor,
-                  borderRadius: BorderRadius.circular(100),
-                ),
-                child: SvgPicture.asset(
-                  width: 20, height: 20,
-                  'assets/icons/ic_cancel.svg',
+              ],
+            ),
+            Positioned(
+              top: 8,
+              right: 8,
+              child: GestureDetector(
+                onTap: () {
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                },
+                child: Container(
+                  width: 24,
+                  height: 24,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).disabledColor,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: SvgPicture.asset(
+                    width: 20,
+                    height: 20,
+                    'assets/icons/ic_cancel.svg',
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
