33031ed56ebee1de1b1e8c541db6bf4ccb2856cb
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-11-05 13:04:32 +0200

    Remove ndkVersion from build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.thechance.sellio.sellio"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
